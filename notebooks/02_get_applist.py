677e4c712bd0ef609d20989f7e90cc47e225d663
Saurav Thapa <saurav@Sauravs-MacBook-Air.local>
2026-05-26 17:36:56 -0400

    Completed, gets a max list of games

--- notebooks/02_get_applist.py
+++ notebooks/02_get_applist.py
@@ -1,25 +1,19 @@
 import requests
 import time
-import pandas
+import pandas as pd
 import os
 from dotenv import load_dotenv
 
-def get_games_list():
-    """
-    Fetches games from Steam 
-    Args:
+# API Key stored in .env to avoid exposing credentials in the repo
+load_dotenv()
+api_key = os.getenv("STEAM_API_KEY")
 
-    Returns:
-        : list of games with details
-    """
-    # Loads variables from .env into the environment and retrieves the API Key
-    load_dotenv()
-    api_key = os.getenv("STEAM_API_KEY")
+# Retrieves a max of 100 games at a time from Steam
+steam_url = f"https://api.steampowered.com/IStoreService/GetAppList/v1/?key={api_key}&max_results=100"
+steam_response = requests.get(steam_url)
+data = steam_response.json()["response"]["apps"]
+print(f"Retrieved {len(data)} games")
 
-    steam_url = f"https://api.steampowered.com/IStoreService/GetAppList/v1/?key={api_key}&max_results=10"
-    steam_response = requests.get(steam_url)
-    data = steam_response.json()
-    
-    print(data["response"]["apps"])
-
-get_games_list()
+# Saves to csv so no refetching later
+df = pd.DataFrame(data)
+df.to_csv("data/raw/app_list.csv", index=False)
