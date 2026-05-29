fa412b9e0975c3964d27942b3ed48197d917bb7a
Saurav Thapa <saurav@Sauravs-MacBook-Air.local>
2026-05-28 21:10:41 -0400

    added function to append to file after each iteration and improved readibility

--- notebooks/ingest_batch.py
+++ notebooks/ingest_batch.py
@@ -1,45 +1,83 @@
 import pandas as pd
-from ingest import get_Data
+import os
+from ingest import get_data
 from get_applist import get_all_apps
+
+# Allows for easier access and change to path if necessary
+APP_LIST_PATH = "data/raw/app_list.csv"
+GAMES_RAW_PATH = "data/raw/games_raw.csv"
+
+# Only retaining fields relevant to launch strategy analysis - drops description, images, legal text, etc
+GAME_KEYS = ["steam_appid", "name", "type", "is_free", "developers", "publishers", "price_overview", "genres", "categories", "release_date", 
+             "recommendations", "metacritic", "platforms"]
+
+def add_games(df_apps, file_exists):
+    """
+    Depending on if file exists, creates new file and adds info or appends info from the next appid
+
+    Args:
+        df_apps (DataFrame): dataframe of all apps
+        file_exists (bool): whether csv file exists or not
+    Returns:
+        game_data (dict): dictionary of game info
+    """
+    # Holds data for all games, used to convert to dataframe later
+    game_data = {}
+
+    if file_exists:
+        # Find where to resume from by locating the last saved appid in the app list
+        curr_games_raw = pd.read_csv(GAMES_RAW_PATH)
+        last_game_added = curr_games_raw["steam_appid"].iloc[-1]
+        last_game_row = (df_apps["appid"] == last_game_added).idxmax()
+        start_idx = last_game_row
+    else:
+        start_idx = -1
+    
+    # Limit iterations to save in batches
+    attempts = 1000
+    for i in range(attempts):
+        start_idx += 1
+        curr_game = get_data(df_apps.iloc[start_idx, 0])
+
+        # Some app IDs are invalid - skip them
+        if curr_game is None:
+            continue
+
+        # Only include games
+        if curr_game.get("type") != "game":
+            continue
+
+        # Skip games with fewer than 50 reviews - insufficient data for meaningful analysis
+        if curr_game.get("recommendations", {}).get("total", 0) < 50:
+            continue
+
+        # Game cannot be unreleased
+        if curr_game.get("release_date", {}).get("coming_soon", True):
+            continue
+
+        # Dictionary comprehension to extract only the required fields
+        game_data[df_apps.iloc[start_idx, 0]] = {key:curr_game.get(key) for key in GAME_KEYS}
+
+        if len(game_data) % 50 == 0:
+            print(f"Fetched {len(game_data)} so far...")
+    
+    print(f"Completed: {len(game_data)} games added from {attempts} attempts")
+
+    return game_data
 
 # Pagination state - loop until Steam confirms no more results
 have_more_results = True
 last_appid = None
-while have_more_results:
-    have_more_results, last_appid = get_all_apps(last_appid)
+
+# Re-fetching the list everytime is redundant
+if not os.path.exists(APP_LIST_PATH):
+    while have_more_results:
+        have_more_results, last_appid = get_all_apps(last_appid)
 
 # Application list generated above - contains all Steam app IDs
-app_list = pd.read_csv("data/raw/app_list.csv")
+df_apps = pd.read_csv(APP_LIST_PATH)
 
-# Holds data for all games, used to convert to dataframe later
-game_data = {}
-
-# Only retaining fields relevant to launch strategy analysis - drops description, images, legal text, etc
-game_keys = ["steam_appid", "name", "type", "is_free", "developers", "publishers", "price_overview", "genres", "categories", "release_date", "recommendations", "metacritic", "platforms"]
-
-for appid in app_list["appid"]:
-    curr_game = get_Data(appid)
-
-    # Some app IDs are invalid - skip them
-    if curr_game is None:
-        continue
-
-    # Skip games with fewer than 50 reviews - insufficient data for meaningful analysis
-    if curr_game.get("recommendations", {}).get("total", 0) < 50:
-        continue
-
-    # Game cannot be unreleased
-    if curr_game.get("release_date", {}).get("coming_soon", True):
-        continue
-
-    # Dictionary comprehension to extract only the required fields
-    game_data[appid] = {key:curr_game.get(key) for key in game_keys}
-
-    if len(game_data) > 0 and len(game_data) % 50 == 0:
-        print(f"Fetched {len(game_data)} so far...")
-
-print(f"Completed: {len(game_data)} games fetched successfully")
-
-# Persist raw data locally to avoid re-fetching from API
-df = pd.DataFrame.from_dict(game_data, orient="index")
-df.to_csv("data/raw/games_raw.csv", index=False)
+# Persist raw data locally to allow re-fetching for other games
+file_exists = os.path.exists(GAMES_RAW_PATH)
+df = pd.DataFrame.from_dict(add_games(df_apps, file_exists), orient="index")
+df.to_csv(GAMES_RAW_PATH, mode="a", index=False, header=(not file_exists))
